3984de712e8c43b5f8f58df457b7756251a442c3
CHARLY_TRICKS <charly@tricks.dev>
2026-07-25 15:03:40 +0000

    fix: import android.view.View

--- ghostvpn/app/src/main/java/com/ghostvpn/MainActivity.kt
+++ ghostvpn/app/src/main/java/com/ghostvpn/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ghostvpn
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
